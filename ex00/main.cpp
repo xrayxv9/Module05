2d8f1a806c4d90b826303561a32e4f6c01cbe09f
Clement Morel <cmorel@3G3.42angouleme.fr>
2025-07-17 13:13:46 +0200

    ex00 cleaned

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,28 +4,43 @@ int main(void)
 {
 	Bureaucrat steph = Bureaucrat();
 	Bureaucrat bernard = Bureaucrat("bernard", 1);
-	// Bureaucrat error = Bureaucrat("bernard", 0);
-	// Bureaucrat error = Bureaucrat("bernard", 151);
 
-	// try
-	// {
-	// 	steph->decrement();
-	// }
-	// catch (Bureaucrat::GradeTooLowException &e)
-	// {
-	// 	std::cerr << "\033[1;031mError: " << e.what() << " detected while decrementing " << steph->getName() << "\033[1;0m" << std::endl;
-	// }
-	//
-	// try
-	// {
-	// 	bernard->increment();
-	// }
-	// catch (Bureaucrat::GradeTooHighException &e)
-	// {
-	// 	std::cerr << "\033[1;031mError: " << e.what() << " detected while incrementing " << bernard->getName() << "\033[1;0m"<< std::endl;
-	// }
+
+	try
+	{
+		Bureaucrat error = Bureaucrat("bernard", 0);
+	}
+	catch (Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << e.what() << std::endl; 
+	}
+
+	try
+	{
+		Bureaucrat error2 = Bureaucrat("bernard", 151);
+	}
+	catch (Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << e.what() << std::endl; 
+	}
+
+	try
+	{
+		steph.decrement();
+	}
+	catch (Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << "\033[1;031mError: " << e.what() << " detected while decrementing " << steph.getName() << "\033[1;0m" << std::endl;
+	}
+	try
+	{
+		bernard.increment();
+	}
+	catch (Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << "\033[1;031mError: " << e.what() << " detected while incrementing " << bernard.getName() << "\033[1;0m"<< std::endl;
+	}
 
 	std::cout << bernard << std::endl;
 	std::cout << steph << std::endl;
-	// std::cout << error << std::endl;
 }
